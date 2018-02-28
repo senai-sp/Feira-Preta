c2f4d374c76699faa1b035180bd2f6cbb2d83bb7
RAFAEL YOSHIHIRO TANAKA <34667191864@jpmorgan.sp>
2018-02-28 19:04:01 -0300

    componente Button e css

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,8 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
+      </div>*/
+    )
   }
 }
 
