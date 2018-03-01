9b8ede4f7ef70384b2a0ac9abfc6d3879b9fabaa
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-03-01 17:23:10 -0300

    Inclusão da section Features

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Form from './components/Form'
+import Features from './components/Features'
 import './App.css'
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <main>
         <h1 className="title">Destaques</h1>
         <Form />
+        <Features />
       </main>
     )
   }
---
+++ admin/src/components/Features/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Features.css'
+
+const Features = props => (
+    <section className='features'>
+        <h2 className='title'>Destaques Cadastrados</h2>
+        <div>
+            <a href="" class="link">https://www.instagram.com/p/BfbRyIRAmL5/?hl=pt-br&taken-by=feirapretaoficial</a>
+            <i class="fas fa-times-circle"></i>
+        </div>
+    </section>
+)
+
+export default Features
