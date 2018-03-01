38404006c0b6827bf2cdb3fe2f9704bb00723c12
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-03-01 16:56:02 -0300

    Componentes iniciais

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import Navbar from './components/Navbar'
 import Form from './components/Form'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
+      <main>
+        <h1 className="title">Destaques</h1>
         <Form />
-      </div>
+      </main>
     )
   }
 }
--- admin/src/components/Form/index.js
+++ admin/src/components/Form/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FormInput from './FormInput'
 import FormButton from './FormButton'
+import './Form.css'
 
 const Form = props => (
-    <form>
-        <FormInput />
+    <form className='feature-form'>
+        <p>Incluir Destaque</p>
+        <FormInput className='feature-form__input' />
         <FormButton className='button button--secondary' children='Cancelar'/>
         <FormButton className='button button--primary' children='Enviar'/>
     </form>
