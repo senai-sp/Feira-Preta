4de2145b2b3f9baea20f226e9b78b9127ae43bb2
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-02-28 21:44:29 -0300

    início navbar

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
 import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
-      <Form />
+      <div>
+        <Navbar />
+        <Form />
+      </div>
     )
   }
 }
---
+++ admin/src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => (
+    <header className="header">
+        <nav>
+            <ul className="navbar">
+                <li>Home</li>
+                <li>Empreendedores</li>
+                <li>Destaques</li>
+            </ul>
+        </nav>
+    </header>
+)
+
+export default Navbar
