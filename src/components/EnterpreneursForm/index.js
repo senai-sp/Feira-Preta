ed5e28294d04439672c6fc9c2604df242f599c9e
YVES LEONARDO CARRASCO PEREIRA <39366274870@jpmorgan.sp>
2018-03-07 19:21:07 -0300

    first commit from this branch! :D

---
+++ src/components/EnterpreneursForm/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FormInput from '../Form/FormInput'
+import FormButton from '../Form/FormButton'
+
+
+const EnterpreneursForm = () => (
+    <form>
+        <FormInput />
+        <FormInput />
+        <FormButton />
+    </form>
+)
+
+export default EnterpreneursForm
--- src/components/Pages/Entrepreneurs/index.js
+++ src/components/Pages/Entrepreneurs/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Entrepreneurs.css'
-
+import EnterpreneursForm from '../../EnterpreneursForm'
 
 const Entrepreneurs = () => (
     <article className="entrepreneurs">
         <h1 className="entrepreneurs__title">Empreendores</h1>
-    </article>  
+        <EnterpreneursForm />
+    </article> 
 )
 
 export default Entrepreneurs;
