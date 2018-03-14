db2f1f7c0b130bf3e2ccca66fc1ceab1c345b070
YVES LEONARDO CARRASCO PEREIRA <39366274870@jpmorgan.sp>
2018-03-14 20:34:03 -0300

    CSS changes on card elements

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import FaTimesCircleO from 'react-icons/lib/fa/times-circle-o'
+import FaTimesCircleO from 'react-icons/lib/fa/times-circle-o'
 import './card.css'
 
 
 const Card = props => (
     <div className="card">
-        {/*<button className="card__delete" type="button" onClick="">
-            <i className="fas fa-times-circle" aria-hidden="true"></i>
-        </button>*/}
+        <button className="card__delete" type="button" onClick="">
+            <FaTimesCircleO className="fas fa-times-circle" aria-hidden="true"></FaTimesCircleO>
+        </button>
         <h3 className="card__user">{props.user}</h3>
         <img className="card__image" src={props.image} alt={props.text} />
         <p className="card__text">{props.text}</p>
