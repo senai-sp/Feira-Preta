3bd849034602183c73b6db48158f3e9918fcb40b
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-03-28 19:54:32 -0300

    Correção do menu hamburguer (abrir e fechar)

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,44 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import classnames from 'classnames'
 import Toaster from '../Toaster'
 import LogoFFP from './logo-FFP.png'
 import FaBars from 'react-icons/lib/fa/bars'
+import FaClose from 'react-icons/lib/fa/close'
 import './Navbar.css'
 
 class Navbar extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { toggleMenu: false };
-        this.toggleClick = this.toggleClick.bind(this);
+        this.state = { menuOpen: false };
+        this.toggleClick = this.toggleClick.bind(this)
     }
 
-    toggleClick() {
-        this.setState(state => ({
-            toggleMenu: !state.toggleMenu
-        }));
+    toggleClick(event) {
+        event.stopPropagation()
+        this.setState(state => ({ menuOpen: !state.menuOpen }))
     }
 
     render() {
+        
         return (
             <header>
                 <nav className="navbar">
+                    <div className={classnames({ 'switch': this.state.menuOpen, 'switch--off': !this.state.menuOpen })} onClick={this.state.menuOpen ? this.toggleClick : undefined} ></div>
                     <img className="navbar__logo" src={LogoFFP} alt="Logo Feira Preta" />
-
-                    <div>
-                        <nav classname={this.state.toggleMenu ? 'navbar-links-open' : 'navbar-links'}>
-                            <Link className="link" to="/">
-                                Home
+                    <nav className={this.state.menuOpen ? 'navbar-links-open' : 'navbar-links'}>
+                        <Link className="link" to="/" onClick={this.toggleClick}>
+                            Home
                             </Link>
-                            <Link className="link" to="/empreendedores">
-                                Empreendedores
+                        <Link className="link" to="/empreendedores" onClick={this.toggleClick}>
+                            Empreendedores
                             </Link>
-                            <Link className="link" to="/destaques">
-                                Destaques
+                        <Link className="link" to="/destaques" onClick={this.toggleClick}>
+                            Destaques
                             </Link>
-                        </nav>
-                        <button className="hamburguer-button" onClick={this.toggleClick}><FaBars className="fas fa-bars" aria-hidden="true"></FaBars></button>
-                    </div>
+                    </nav>
+                    <button className="hamburguer-button" onClick={this.toggleClick}>{this.state.menuOpen ? <FaClose className="fas fa-close" aria-hidden="true"></FaClose> : <FaBars className="fas fa-bars" aria-hidden="true"></FaBars>}</button>
                 </nav>
                 <Toaster />
             </header>
@@ -46,8 +45,4 @@ class Navbar extends React.Component {
     }
 }
 
-const mapStateToProps = state => ({
-    navbar: state.navbar
-})
-
-export default connect(mapStateToProps, null)(Navbar)
+export default Navbar
