25e8eb81ebe2c6341512cda1b391a78ee1152db4
Yves Carrasco <yvescarrasco7@gmail.com>
2018-03-28 21:11:00 -0300

    dashboard layout

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Dashboard from '../../Dashboard'
 import './Home.css'
 
 
 const Home = () => (
     <article className="home">
         <h1 className="home__title">Home</h1>
+        <Dashboard />
     </article>  
 )
 
