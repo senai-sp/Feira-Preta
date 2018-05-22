386cf488c346eff8cdfc73e774b93b3d81d15a04
Kevin Dantas <kevinds29@gmail.com>
2018-05-21 21:18:57 -0300

    Fix url

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL: 'http://feirapreta-001-site1.ctempurl.com/api',
-    baseURL: 'http://http://feira-black.herokuapp.com/api'
+    baseURL: 'http://feira-black.herokuapp.com/api'
 })
 
 export default instance
